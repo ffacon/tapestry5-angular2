a520f351cbcc4c285d2cc1e21eddc37527aedb49
ffacon <francois.facon@gmail.com>
2016-02-16 00:05:55 +0100

    Add Angular2Symbols

---
+++ src/main/java/org/got5/tapestry5/angular2/services/Angular2SymbolConstants.java
@@ -0,0 +1,13 @@
+package org.got5.tapestry5.angular2.services;
+
+
+public class Angular2SymbolConstants {
+	
+	/**
+     * The root asset path for angular2.
+     *
+     * @since 5.4
+     */
+    public static final String ANGULAR2_ROOT = "tapestry.angular2-root";
+
+}
