0d2dc69b35f13db18a88abb642d598e82f67d881
ffacon <francois.facon@gmail.com>
2016-02-28 23:23:49 +0100

    Adding helloworld/app.js

---
+++ src/test/resources/META-INF/modules/helloworld/app.js
@@ -0,0 +1,40 @@
+System.register(["angular2/platform/browser", "angular2/core"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var browser_1, core_1;
+    var HelloWorld;
+    return {
+        setters:[
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            HelloWorld = (function () {
+                function HelloWorld() {
+                }
+                HelloWorld = __decorate([
+                    core_1.Component({
+                        selector: 'hello-world',
+                        template: "\n  <div>\n    Hello world\n  </div>\n  "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], HelloWorld);
+                return HelloWorld;
+            }());
+            browser_1.bootstrap(HelloWorld);
+        }
+    }
+});
+//# sourceMappingURL=app.js.map
