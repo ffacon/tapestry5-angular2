daf08c14a7386e0654e6fe8fbeb10895a847e8ef
ffacon <francois.facon@gmail.com>
2016-02-29 21:31:42 +0100

    Adding Index Page

---
+++ src/test/java/org/got5/tapestry5/angular2/pages/Index.java
@@ -0,0 +1,15 @@
+package org.got5.tapestry5.angular2.pages;
+
+
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.annotations.AfterRender;
+import org.apache.tapestry5.services.javascript.InitializationPriority;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+
+import javax.inject.Inject;
+
+public class Index {
+
+
+
+}	
