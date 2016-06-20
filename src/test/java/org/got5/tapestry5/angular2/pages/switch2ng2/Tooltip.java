d712812e641a6b3463726f90f2ad9867d089c26a
ffacon <francois.facon@gmail.com>
2016-06-20 21:04:19 +0200

    add tooltip page

---
+++ src/test/java/org/got5/tapestry5/angular2/pages/switch2ng2/Tooltip.java
@@ -0,0 +1,10 @@
+package org.got5.tapestry5.angular2.pages.switch2ng2;
+
+import org.apache.tapestry5.annotations.Import;
+
+@Import( stylesheet ={"context:ngbook2/redditapp/resources/vendor/semantic.min.css"})
+public class Tooltip {
+	
+	
+
+}
